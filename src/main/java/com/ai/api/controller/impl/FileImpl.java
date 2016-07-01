16b46de725d9a812e591e941d566ced6054bf52d
Allen <actanzhang@gmail.com>
2016-07-01 13:02:04 +0800

    AI-2527, clean code

--- src/main/java/com/ai/api/controller/impl/FileImpl.java
+++ src/main/java/com/ai/api/controller/impl/FileImpl.java
@@ -1,88 +1,79 @@
 package com.ai.api.controller.impl;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletResponse;
+
 import com.ai.api.bean.FileDetailBean;
 import com.ai.api.controller.File;
 import com.ai.api.exception.AIException;
 import com.ai.api.service.FileService;
 import com.ai.commons.annotation.TokenSecured;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import javax.imageio.ImageIO;
-import javax.servlet.ServletOutputStream;
-import javax.servlet.http.HttpServletResponse;
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 /***************************************************************************
  * <PRE>
  * Project Name    : api
- * <p>
+ * <p/>
  * Package Name    : com.ai.api.controller.impl
- * <p>
+ * <p/>
  * Creation Date   : 2016/6/30 12:20
- * <p>
+ * <p/>
  * Author          : Jianxiong Cai
- * <p>
+ * <p/>
  * Purpose         : TODO
- * <p>
- * <p>
+ * <p/>
+ * <p/>
  * History         : TODO
- * <p>
+ * <p/>
  * </PRE>
  ***************************************************************************/
 
 @RestController
 public class FileImpl implements File {
 
-    private static final Logger logger = LoggerFactory.getLogger(FileImpl.class);
+	private static final Logger logger = LoggerFactory.getLogger(FileImpl.class);
 
-    @Autowired
-    private FileService fileService;
+	@Autowired
+	private FileService fileService;
 
-    @Override
-    @TokenSecured
-    @RequestMapping(value = "/user/{userId}/file/{fileId}/detail", method = RequestMethod.GET)
-    public ResponseEntity<FileDetailBean> getFileDetailInfo(@PathVariable("userId") String userId,
-                                                            @PathVariable("fileId") String fileId)
-            throws IOException, AIException {
-        FileDetailBean fileDetailBean = null;
-        try{
-            fileDetailBean = fileService.getFileDetailInfo(userId,fileId);
-        }catch (Exception e){
-            logger.error("ERROR! from[getFileDetailInfo]:",e);
-        }
+	@Override
+	@TokenSecured
+	@RequestMapping(value = "/user/{userId}/file/{fileId}/detail", method = RequestMethod.GET)
+	public ResponseEntity<FileDetailBean> getFileDetailInfo(@PathVariable("userId") String userId,
+	                                                        @PathVariable("fileId") String fileId)
+			throws IOException, AIException {
+		FileDetailBean fileDetailBean = null;
+		try {
+			fileDetailBean = fileService.getFileDetailInfo(userId, fileId);
+		} catch (Exception e) {
+			logger.error("ERROR! from[getFileDetailInfo]:", e);
+		}
 
-        if (null== fileDetailBean) {
-            logger.info("Null result!from[getFileDetailInfo]");
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-        return new ResponseEntity<>(fileDetailBean, HttpStatus.OK);
-    }
+		if (null == fileDetailBean) {
+			logger.info("Null result!from[getFileDetailInfo]");
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<>(fileDetailBean, HttpStatus.OK);
+	}
 
-    @Override
-//    @ResponseBody
-    @RequestMapping(value = "/user/{userId}/file/{fileId}", method = RequestMethod.GET)
-    public void downloadFile(@PathVariable("userId") String userId, @PathVariable("fileId") String fileId,
-                             HttpServletResponse httpResponse) {
-        try {
-            boolean b = fileService.downloadFile(userId,fileId,httpResponse);
-            logger.info("success!");
-        }catch (Exception e){
-            logger.error("ERROR! from[downloadFile]:",e);
-        }
-    }
+	@Override
+	@RequestMapping(value = "/user/{userId}/file/{fileId}", method = RequestMethod.GET)
+	public void downloadFile(@PathVariable("userId") String userId, @PathVariable("fileId") String fileId,
+	                         HttpServletResponse httpResponse) {
+		try {
+			boolean b = fileService.downloadFile(userId, fileId, httpResponse);
+			logger.info("success!");
+		} catch (Exception e) {
+			logger.error("ERROR! from[downloadFile]:", e);
+		}
+	}
 }
