68fbaf18e12f163c29e88869363fdb8b6f61f937
Allen <actanzhang@gmail.com>
2016-09-08 14:44:27 +0800

    Revert "add method for cache expirei"
    This reverts commit 067f169a57a675891604e315e07f2957b9303aab.

--- src/main/java/com/ai/api/util/RedisUtil.java
+++ src/main/java/com/ai/api/util/RedisUtil.java
@@ -1,13 +1,13 @@
 package com.ai.api.util;
 
-import java.util.List;
-import java.util.concurrent.locks.ReentrantLock;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
+
+import java.util.List;
+import java.util.concurrent.locks.ReentrantLock;
 
 /***************************************************************************
  * <PRE>
@@ -34,14 +34,12 @@ public class RedisUtil {
 	protected static ReentrantLock lockPool = new ReentrantLock();
 	protected static ReentrantLock lockJedis = new ReentrantLock();
 
-	private static RedisUtil instance;
+	private static RedisUtil instance ;
 	//private static Jedis jedis;
 	//private JedisPool pool = null;
 	private static JedisPool pool = null;
 
-	public static final int hour = 60 * 60;         //1 hour
-
-	public RedisUtil() {
+	public RedisUtil(){
 		/*
 		try{
 //			jedis = new Jedis("202.66.128.138", 6379);
@@ -63,46 +61,39 @@ public class RedisUtil {
 		//jedis = this.getJedis();
 	}
 
-	/**
-	 * init the redis conneciton pool
-	 */
-	private static void initialPool() {
+	/** * 初始化Redis连接池 */
+	private static void initialPool(){
 		try {
 			JedisPoolConfig config = new JedisPoolConfig();
 			//config.setMaxTotal(-1);
 			//config.setMaxIdle(8);
 			//config.setMaxWaitMillis(100000);
 			config.setTestOnBorrow(true);
-			pool = new JedisPool(config, "202.66.128.138", 6379, 100000, "aiitteam");
+			pool = new JedisPool(config, "202.66.128.138", 6379, 100000,"aiitteam");
 		} catch (Exception e) {
-			logger.error("First create JedisPool error : " + e);
+			logger.error("First create JedisPool error : "+e);
 		}
 	}
 
-	/**
-	 * pool init
-	*/
+	/** * 在多线程环境同步初始化 */
 	private static synchronized void poolInit() {
-		//if locked then do nothing
-		assert !lockPool.isHeldByCurrentThread();
+		//断言 ，当前锁是否已经锁住，如果锁住了，就啥也不干，没锁的话就执行下面步骤
+		assert ! lockPool.isHeldByCurrentThread();
 		lockPool.lock();
 		try {
 			if (pool == null) {
 				initialPool();
 			}
-		} catch (Exception e) {
+		}catch(Exception e){
 			e.printStackTrace();
 		} finally {
 			lockPool.unlock();
 		}
 	}
 
-	/**
-	 * get redis instance
-	 *
-	 */
+	/** * 同步获取Jedis实例 * @return Jedis */
 	public synchronized static Jedis getJedis() {
-		assert !lockJedis.isHeldByCurrentThread();
+		assert ! lockJedis.isHeldByCurrentThread();
 		lockJedis.lock();
 
 		if (pool == null) {
@@ -114,95 +105,70 @@ public class RedisUtil {
 				jedis = pool.getResource();
 			}
 		} catch (Exception e) {
-			logger.error("Get jedis error : " + e);
-		} finally {
+			logger.error("Get jedis error : "+e);
+		} finally{
 			//returnResource(jedis2);
 			lockJedis.unlock();
 		}
 		return jedis;
 	}
 
-	/**
-	 * release redis resource
-	 *
-	 * @param jedis
-	 */
+	/** * 释放jedis资源 * @param jedis */
 	public static void returnResource(final Jedis jedis) {
-		if (jedis != null && pool != null) {
+		if (jedis != null && pool !=null) {
 			pool.returnResource(jedis);
 		}
 	}
 
 
 	public static synchronized RedisUtil getInstance() {
-		if (instance == null) {
+		if(instance == null){
 			instance = new RedisUtil();
 		}
 		return instance;
 	}
 
-	public synchronized static boolean exists(String key) {
+	public synchronized static boolean exists(String key){
 		Jedis jedis = getJedis();
 		try {
 			return jedis.exists(key.trim());
 		} catch (Exception e) {
-			logger.error("key exists error : " + e);
+			logger.error("key exists error : "+e);
 			return false;
 		} finally {
 			returnResource(jedis);
 		}
 	}
-
 	/**
 	 * Set the string value as value of the key. The string can't be longer than 1073741824 bytes (1GB).
-	 *
 	 * @param key
 	 * @param value
 	 * @return
 	 */
-	public synchronized static String set(String key, String value) {
+	public synchronized static String set( String key,  String value){
 		Jedis jedis = getJedis();
 		try {
 			return jedis.set(key.trim(), value.trim());
 		} catch (Exception e) {
-			logger.error("key set error : " + e);
+			logger.error("key set error : "+e);
 			return null;
 		} finally {
 			returnResource(jedis);
 		}
 	}
-
-	public synchronized static String setWithExpire(final String key, final String value, int expiry) {
-		Jedis jedis = getJedis();
-		try {
-			String result = jedis.set(key.trim(), value.trim());
-			jedis.expire(key.trim(), expiry);
-			return result;
-		} catch (Exception e) {
-			logger.error("key set error : " + e);
-			return null;
-		} finally {
-			returnResource(jedis);
-		}
-
-	}
-
-
-
 	/**
 	 * Get the value of the specified key. If the key does not exist null is returned. If the value
 	 * stored at key is not a string an error is returned because GET can only handle string values.
-	 *
 	 * @param key
 	 * @return
 	 */
-	public synchronized static String get(String key) {
+	public synchronized static String get(String key){
 		Jedis jedis = getJedis();
 		try {
 			if (!exists(key.trim())) return null;
 			return String.valueOf((jedis.get(key.trim())));
 		} catch (Exception e) {
-			logger.error("key get error : " + e);
+			logger.error("key get error : "+e);
 			return null;
 		} finally {
 			returnResource(jedis);
@@ -212,16 +178,14 @@ public class RedisUtil {
 	/**
 	 * Remove the specified keys. If a given key does not exist no operation is performed for this
 	 * key. The command returns the number of keys removed.
-	 *
 	 * @param key
 	 */
-
-	public synchronized static Long del(String key) {
+	public synchronized static Long del(String key){
 		Jedis jedis = getJedis();
-		try {
+		try{
 			return jedis.del(key.trim());
-		} catch (Exception e) {
-			logger.error("key del error : " + e);
+		}catch (Exception e) {
+			logger.error("key del error : "+e);
 			return null;
 		} finally {
 			returnResource(jedis);
@@ -229,62 +193,60 @@ public class RedisUtil {
 	}
 
 	/**
+	 *
 	 * Set the specified hash field to the specified value.
 	 * If key does not exist, a new key holding a hash is created.
-	 *
 	 * @param key
 	 * @param fieId
 	 * @param value
 	 * @return
 	 */
-	public synchronized static Long hset(String key, String fieId, String value) {
+	public synchronized static Long hset(String key,String fieId,String value){
 		Jedis jedis = getJedis();
 		try {
 			return jedis.hset(key.trim(), fieId.trim(), value.trim());
-		} catch (Exception e) {
-			logger.error("key hset error : " + e);
+		}catch (Exception e) {
+			logger.error("key hset error : "+e);
 			return null;
 		} finally {
 			returnResource(jedis);
 		}
 	}
 
-	/**
-	 * Return all the values in a hash.
-	 * <p>
-	 * <b>Time complexity:</b> O(N), where N is the total number of entries
-	 *
-	 * @param key
-	 * @return All the fields values contained into a hash.
-	 */
-	public synchronized static List<String> hvals(String key) {
-		Jedis jedis = getJedis();
-		try {
-			if (!exists(key.trim())) return null;
-			return jedis.hvals(key.trim());
-		} catch (Exception e) {
-			logger.error("key hvals error : " + e);
-			return null;
-		} finally {
-			returnResource(jedis);
-		}
-	}
+    /**
+     * Return all the values in a hash.
+     * <p>
+     * <b>Time complexity:</b> O(N), where N is the total number of entries
+     * @param key
+     * @return All the fields values contained into a hash.
+     */
+    public synchronized static List<String> hvals(String key){
+        Jedis jedis = getJedis();
+        try {
+            if (!exists(key.trim())) return null;
+            return jedis.hvals(key.trim());
+        } catch (Exception e) {
+            logger.error("key hvals error : "+e);
+            return null;
+        } finally {
+            returnResource(jedis);
+        }
+    }
 
 	/**
 	 * If key holds a hash, retrieve the value associated to the specified field.
 	 * If the field is not found or the key does not exist, a special 'nil' value is returned.
-	 *
 	 * @param key
 	 * @param fieId
 	 * @return
 	 */
-	public synchronized static String hget(String key, String fieId) {
+	public synchronized static String hget(String key,String fieId){
 		Jedis jedis = getJedis();
 		try {
 			if (!exists(key.trim())) return null;
 			return jedis.hget(key.trim(), fieId.trim());
 		} catch (Exception e) {
-			logger.error("key hget error : " + e);
+			logger.error("key hget error : "+e);
 			return null;
 		} finally {
 			returnResource(jedis);
@@ -295,25 +257,29 @@ public class RedisUtil {
 	 * Remove the specified field from an hash stored at key.
 	 * If the field was present in the hash it is deleted and 1 is returned,
 	 * otherwise 0 is returned and no operation is performed.
-	 *
 	 * @param key
 	 */
-	public synchronized static Long hdel(String key, String fieId) {
+	public synchronized static Long hdel(String key,String fieId){
 		Jedis jedis = getJedis();
 		try {
 			return jedis.hdel(key.trim(), fieId.trim());
 		} catch (Exception e) {
-			logger.error("key hdel error : " + e);
+			logger.error("key hdel error : "+e);
 			return null;
 		} finally {
 			returnResource(jedis);
 		}
 	}
 
-	private synchronized static void flushAll() {
+	private synchronized static void flushAll(){
 		Jedis jedis = getJedis();
 		jedis.flushAll();
 	}
+
+
+
+
+
 
 	public static void main(String[] args) {
 		RedisUtil ru = RedisUtil.getInstance();
