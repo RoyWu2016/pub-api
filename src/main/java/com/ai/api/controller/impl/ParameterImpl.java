dbedd07a06a1d83af4d9cbfafebb54652c6c2d87
Roy Wu <Roy Wu@ldj-PC>
2016-09-20 11:15:45 +0800

    fixed bugs for AI-3041

--- src/main/java/com/ai/api/controller/impl/ParameterImpl.java
+++ src/main/java/com/ai/api/controller/impl/ParameterImpl.java
@@ -1,19 +1,10 @@
 package com.ai.api.controller.impl;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import com.ai.api.bean.ProductCategoryDtoBean;
-import com.ai.api.bean.ProductFamilyDtoBean;
-import com.ai.api.controller.Parameter;
-import com.ai.api.service.ParameterService;
-import com.ai.commons.annotation.TokenSecured;
-import com.ai.commons.beans.checklist.vo.CKLDefectVO;
-import com.ai.commons.beans.checklist.vo.CKLTestVO;
-import com.ai.commons.beans.params.ChecklistTestSampleSizeBean;
-import com.ai.commons.beans.params.ClassifiedBean;
-import com.ai.commons.beans.params.product.SysProductTypeBean;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +13,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ai.api.bean.ProductCategoryDtoBean;
+import com.ai.api.bean.ProductFamilyDtoBean;
+import com.ai.api.bean.TextileProductCategoryBean;
+import com.ai.api.controller.Parameter;
+import com.ai.api.service.ParameterService;
+import com.ai.commons.annotation.TokenSecured;
+import com.ai.commons.beans.checklist.vo.CKLDefectVO;
+import com.ai.commons.beans.checklist.vo.CKLTestVO;
+import com.ai.commons.beans.params.ChecklistTestSampleSizeBean;
+import com.ai.commons.beans.params.ClassifiedBean;
+import com.ai.commons.beans.params.product.SysProductTypeBean;
 
 /**
  * Created by Administrator on 2016/6/21 0021.
@@ -138,7 +141,7 @@ public class ParameterImpl implements Parameter {
 	@Override
 	@TokenSecured
 	@RequestMapping(value = "/parameter/textile-product-categories", method = RequestMethod.GET)
-	public ResponseEntity<List<ClassifiedBean>> getTextileProductCategories() {
+	public ResponseEntity<List<TextileProductCategoryBean>> getTextileProductCategories() {
 		// TODO Auto-generated method stub
 		logger.info("get getTextileProductCategory");
 		List<ClassifiedBean> result = parameterService.getTextileProductCategories();
@@ -146,6 +149,14 @@ public class ParameterImpl implements Parameter {
 			logger.error("getTextileProductCategory not found");
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 		}
-		return new ResponseEntity<>(result, HttpStatus.OK);
+		List<TextileProductCategoryBean> list = new ArrayList<TextileProductCategoryBean>();
+		for(ClassifiedBean each : result) {
+			TextileProductCategoryBean bean = new TextileProductCategoryBean();
+			bean.setLabel(each.getKey());
+			bean.setValue(each.getValue());
+			
+			list.add(bean);
+		}
+		return new ResponseEntity<>(list, HttpStatus.OK);
 	}
 }
