b0a7e14815d0d9770199beb237b8b7dd01ea7969
Roy Wu <Roy Wu@ldj-PC>
2016-09-29 17:15:18 +0800

    COMMIT AI-3121

--- src/main/java/com/ai/api/service/FactoryService.java
+++ src/main/java/com/ai/api/service/FactoryService.java
@@ -1,12 +1,12 @@
 package com.ai.api.service;
 
+import java.io.IOException;
+import java.util.List;
+
 import com.ai.api.bean.SupplierDetailBean;
 import com.ai.api.bean.legacy.FactorySearchBean;
 import com.ai.api.exception.AIException;
-import com.ai.commons.beans.ServiceCallResult;
-
-import java.io.IOException;
-import java.util.List;
+import com.ai.commons.beans.psi.OrderFactoryBean;
 
 /**
  * Created by Administrator on 2016/6/29 0029.
@@ -20,4 +20,6 @@ public interface FactoryService {
     boolean updateSupplierDetailInfo(SupplierDetailBean supplierDetailBean) throws IOException, AIException;
     boolean deleteSuppliers(String supplierIds) throws IOException, AIException;
     String createSupplier(SupplierDetailBean supplierDetailBean) throws IOException, AIException;
+
+	boolean supplierConfirmOrder(String orderId, String inspectionDateString, String containReadyTime, OrderFactoryBean orderFactoryBean);
 }
--- src/main/java/com/ai/api/service/impl/FactoryServiceImpl.java
+++ src/main/java/com/ai/api/service/impl/FactoryServiceImpl.java
@@ -8,6 +8,8 @@ import com.ai.api.bean.legacy.FactorySearchBean;
 import com.ai.api.dao.FactoryDao;
 import com.ai.api.exception.AIException;
 import com.ai.api.service.FactoryService;
+import com.ai.commons.beans.psi.OrderFactoryBean;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,31 +23,32 @@ import org.springframework.stereotype.Service;
 @Service("factoryService")
 public class FactoryServiceImpl implements FactoryService {
 
-    protected Logger logger = LoggerFactory.getLogger(FactoryServiceImpl.class);
+	protected Logger logger = LoggerFactory.getLogger(FactoryServiceImpl.class);
 
-    @Autowired
-    @Qualifier("factoryDao")
-    private FactoryDao factoryDao;
+	@Autowired
+	@Qualifier("factoryDao")
+	private FactoryDao factoryDao;
 
-    @Override
-    public List<FactorySearchBean> getSuppliersByUserId(String userId) throws IOException, AIException {
-        return factoryDao.getSuppliersByUserId(userId);
-    }
+	@Override
+	public List<FactorySearchBean> getSuppliersByUserId(String userId) throws IOException, AIException {
+		return factoryDao.getSuppliersByUserId(userId);
+	}
 
-    @Override
-    public SupplierDetailBean getUserSupplierDetailInfoById(String userId, String supplierId) throws IOException, AIException{
-        return factoryDao.getUserSupplierDetailInfoById(userId, supplierId);
-    }
+	@Override
+	public SupplierDetailBean getUserSupplierDetailInfoById(String userId, String supplierId)
+			throws IOException, AIException {
+		return factoryDao.getUserSupplierDetailInfoById(userId, supplierId);
+	}
 
-    @Override
-    public boolean updateSupplierDetailInfo(SupplierDetailBean supplierDetailBean) throws IOException, AIException{
-        return factoryDao.updateSupplierDetailInfo(supplierDetailBean);
-    }
+	@Override
+	public boolean updateSupplierDetailInfo(SupplierDetailBean supplierDetailBean) throws IOException, AIException {
+		return factoryDao.updateSupplierDetailInfo(supplierDetailBean);
+	}
 
-    @Override
-    public boolean deleteSuppliers(String supplierIds) throws IOException, AIException {
-        return factoryDao.deleteSuppliers(supplierIds);
-    }
+	@Override
+	public boolean deleteSuppliers(String supplierIds) throws IOException, AIException {
+		return factoryDao.deleteSuppliers(supplierIds);
+	}
 
 	@Override
 	public String createSupplier(SupplierDetailBean supplierDetailBean) throws IOException, AIException {
@@ -53,5 +56,11 @@ public class FactoryServiceImpl implements FactoryService {
 		return factoryDao.createSupplier(supplierDetailBean);
 	}
 
+	@Override
+	public boolean supplierConfirmOrder(String orderId, String inspectionDateString, String containReadyTime,
+			OrderFactoryBean orderFactoryBean) {
+		// TODO Auto-generated method stub
+		return factoryDao.supplierConfirmOrder(orderId,inspectionDateString,containReadyTime,orderFactoryBean);
+	}
 
 }
