a5b5db831e12a6cf29de5adfaa48e68e10eb2fb1
Roy Wu <Roy Wu@ldj-PC>
2016-10-17 12:18:10 +0800

    change the logo path for AI-3165

--- src/main/java/com/ai/api/service/impl/ReportServiceImpl.java
+++ src/main/java/com/ai/api/service/impl/ReportServiceImpl.java
@@ -234,8 +234,8 @@ public class ReportServiceImpl implements ReportService {
 
 		sheet.addMergedRegion(new CellRangeAddress(4, 4, 0, 8));
 		
-//		String fileName = config.getExcleLoggoCommonSource() + File.separator + "logo.png";
-		String fileName = "E:" +  File.separator + "logo.png";
+		String fileName = config.getExcleLoggoCommonSource() + File.separator + "logo.png";
+//		String fileName = "E:" +  File.separator + "logo.png";
 		logger.info("fine the logo resource: " + fileName);
 		InputStream is = new FileInputStream(fileName);  
     	byte[] bytes = IOUtils.toByteArray(is);  
