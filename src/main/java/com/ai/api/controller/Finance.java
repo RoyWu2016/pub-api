36df25b2dadab9ee6a80df6d407ab2708aa890ef
ansioncai <ansioncai@qq.com>
2016-12-29 17:57:20 +0800

    AI-3526 | change param type for finance api

--- src/main/java/com/ai/api/controller/Finance.java
+++ src/main/java/com/ai/api/controller/Finance.java
@@ -1,13 +1,7 @@
 package com.ai.api.controller;
 
-import com.ai.api.bean.finance.NSCreditDebitMemo;
-import com.ai.api.bean.finance.NSInvoice;
-import com.ai.api.bean.finance.NSLog;
 import com.ai.commons.beans.ApiCallResult;
 import org.springframework.http.ResponseEntity;
-
-import java.util.List;
-import java.util.Map;
 
 /**
  * Project Name    : Public-API
@@ -19,9 +13,9 @@ import java.util.Map;
  */
 public interface Finance {
 
-    ResponseEntity<ApiCallResult> processNSLog(List<NSLog> nsLogs);
+    ResponseEntity<ApiCallResult> processNSLog(String nsLogs);
 
-    ResponseEntity<ApiCallResult> processCreditOrDebitMemos(Map<String, List<NSCreditDebitMemo>> cndnMap);
+    ResponseEntity<ApiCallResult> processCreditOrDebitMemos(String cndnMap);
 
-    ResponseEntity<ApiCallResult> processInvoice(Map<String, List<NSInvoice>> invMap);
+    ResponseEntity<ApiCallResult> processInvoice(String invMap);
 }
