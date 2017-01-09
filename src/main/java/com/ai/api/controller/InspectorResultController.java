af92148a8296a66910201ea99d0f93ae393ccc08
ansioncai <ansioncai@qq.com>
2017-01-09 12:17:12 +0800

    AI-3568 | move file related logic in ip-service to publicAPI (cherry picked from commit f20ded1)

--- src/main/java/com/ai/api/controller/InspectorResultController.java
+++ src/main/java/com/ai/api/controller/InspectorResultController.java
@@ -1,8 +1,15 @@
 package com.ai.api.controller;
 
+import com.ai.api.controller.impl.InspectorResultControllerImpl;
 import com.ai.commons.beans.ApiCallResult;
 import com.ai.commons.beans.psi.InspResultForm;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Project Name    : Public-API
@@ -22,4 +29,11 @@ public interface InspectorResultController {
     ResponseEntity<ApiCallResult> searchBySourceId(String sourceId, String reportType);
     ResponseEntity<ApiCallResult> searchBySourceType(String sourceId, String sourceType, String reportType);
     ResponseEntity<ApiCallResult> getAllIpSupervisorData(String productId);
+    ResponseEntity<ApiCallResult> uploadFile(String sourceId,String username,MultipartHttpServletRequest request);
+    ResponseEntity<ApiCallResult> uploadFileWithCaption(String sourceId, String username, Map<String,List<InspectorResultControllerImpl.offlineVM>> map);
+    void getFile(String fileIds,HttpServletResponse response) throws IOException;
+    void downloadFileV1(String fileIds,HttpServletResponse response) throws IOException;
+    ResponseEntity<ApiCallResult> deleteFile(String fileId,String userName);
+    ResponseEntity<ApiCallResult> getFileInfo(String fileIds);
+    ResponseEntity<ApiCallResult> getFileName(String fileIds);
 }
