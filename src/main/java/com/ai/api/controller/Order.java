1af8a03b57c55265b6814a1271dc1397ff731de4
ansioncai <ansioncai@qq.com>
2017-02-03 12:02:21 +0800

    UIUX-996 | fix bug for getReInspectionList API only get status='60' (cherry picked from commit 421bcb0)

--- src/main/java/com/ai/api/controller/Order.java
+++ src/main/java/com/ai/api/controller/Order.java
@@ -64,7 +64,7 @@ public interface Order {
 	ResponseEntity<ApiCallResult> getFilesByOrderID(String userId, String orderId) throws IOException;
 
 	ResponseEntity<List<SimpleOrderSearchBean>> getReInspectionList(String userId, String serviceType, String keyword,
-			String orderStatus, String pageSize, String pageNumber);
+			 String pageSize, String pageNumber);
 
 //	ResponseEntity<Map<String, ApiCallResult>> getOrderAction(String userId, String orderId);
 
