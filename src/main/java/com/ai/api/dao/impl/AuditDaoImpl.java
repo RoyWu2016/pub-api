736bff7e719484d64f834495f21c0880deef8edf
Roy Wu <Roy Wu@ldj-PC>
2017-02-20 16:44:52 +0800

    fixed conflict error

--- src/main/java/com/ai/api/dao/impl/AuditDaoImpl.java
+++ src/main/java/com/ai/api/dao/impl/AuditDaoImpl.java
@@ -3,19 +3,19 @@ package com.ai.api.dao.impl;
 import java.io.IOException;
 import java.net.URLDecoder;
 
-import com.ai.api.bean.consts.ConstMap;
-import com.ai.commons.Consts;
-import com.ai.commons.StringUtils;
 import org.apache.commons.lang.exception.ExceptionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 
+import com.ai.api.bean.consts.ConstMap;
 import com.ai.api.config.ServiceConfig;
 import com.ai.api.dao.AuditDao;
+import com.ai.commons.Consts;
 import com.ai.commons.HttpUtil;
 import com.ai.commons.JsonUtil;
+import com.ai.commons.StringUtils;
 import com.ai.commons.beans.ApiCallResult;
 import com.ai.commons.beans.ServiceCallResult;
 import com.ai.commons.beans.audit.api.ApiAuditBookingBean;
@@ -89,90 +89,6 @@ public class AuditDaoImpl implements AuditDao {
 		}
 		return finalResult;
 	}
-
-    @Override
-    public ApiCallResult deleteDrafts(String userId, String companyId, String parentId, String draftIds) {
-        StringBuilder url = new StringBuilder(auditBaseUrl);
-        ApiCallResult finalResult = new ApiCallResult();
-        url.append("/api/audit/deleteDrafts");
-        url.append("?userId=" + userId);
-        url.append("&companyId=" + companyId);
-        url.append("&parentId=" + parentId);
-        url.append("&draftIds=" + draftIds);
-        try {
-            ServiceCallResult result = HttpUtil.issueDeleteRequest(url.toString(),null);
-            finalResult = JsonUtil.mapToObject(result.getResponseString(), ApiCallResult.class);
-        } catch (IOException e) {
-            logger.error("Error deleteDrafts!"+ExceptionUtils.getStackTrace(e));
-            finalResult.setMessage("Exception: " + e.toString());
-        }
-        return finalResult;
-    }
-
-    @Override
-    public ApiCallResult searchDrafts(String userId, String companyId, String parentId, String serviceType,String startDate,String endDate,
-                                      String keyWord,int pageSize,int pageNo) {
-        String type = ConstMap.serviceTypeMap.get(serviceType.toLowerCase());
-        serviceType = type;
-        if (type.indexOf(",")!=-1){
-            serviceType = type.split(",")[0];
-        }
-        StringBuilder url = new StringBuilder(auditBaseUrl);
-        ApiCallResult finalResult = new ApiCallResult();
-        url.append("/api/audit/search-drafts");
-        url.append("?userId=" + userId);
-        url.append("&companyId=" + companyId);
-        url.append("&parentId=" + parentId);
-        url.append("&serviceType=" + serviceType);
-        url.append("&startDate=" + startDate);
-        url.append("&endDate=" + endDate);
-        url.append("&keyWord=" + keyWord);
-        url.append("&pageSize=" + pageSize);
-        url.append("&pageNo=" + pageNo);
-        try {
-            ServiceCallResult result = HttpUtil.issueGetRequest(url.toString(),null);
-            finalResult = JsonUtil.mapToObject(result.getResponseString(), ApiCallResult.class);
-        } catch (IOException e) {
-            logger.error("Error searchDrafts!"+ExceptionUtils.getStackTrace(e));
-            finalResult.setMessage("Exception: " + e.toString());
-        }
-        return finalResult;
-    }
-
-    @Override
-    public ApiCallResult searchOrders(String userId, String companyId, String parentId,
-                                      String serviceType,String startDate,String endDate,
-                                      String orderStatus,String keyWord,int pageSize,int pageNo) {
-        String type = ConstMap.serviceTypeMap.get(serviceType.toLowerCase());
-        String subServiceType = null;
-        serviceType = type;
-        if (type.indexOf(",")!=-1){
-            serviceType = type.split(",")[0];
-            subServiceType = type.split(",")[1];
-        }
-        StringBuilder url = new StringBuilder(auditBaseUrl);
-        ApiCallResult finalResult = new ApiCallResult();
-        url.append("/api/audit/search-orders");
-        url.append("?userId=" + userId);
-        url.append("&companyId=" + companyId);
-        url.append("&parentId=" + parentId);
-        url.append("&serviceType=" + serviceType);
-        url.append("&subServiceType=" + subServiceType);
-        url.append("&startDate=" + startDate);
-        url.append("&endDate=" + endDate);
-        url.append("&keyWord=" + keyWord);
-        url.append("&orderStatus=" + orderStatus);
-        url.append("&pageSize=" + pageSize);
-        url.append("&pageNo=" + pageNo);
-        try {
-            ServiceCallResult result = HttpUtil.issueGetRequest(url.toString(),null);
-            finalResult = JsonUtil.mapToObject(result.getResponseString(), ApiCallResult.class);
-        } catch (IOException e) {
-            logger.error("Error searchOrders!"+ExceptionUtils.getStackTrace(e));
-            finalResult.setMessage("Exception: " + e.toString());
-        }
-        return finalResult;
-    }
 
     @Override
     public ApiCallResult deleteDrafts(String userId, String companyId, String parentId, String draftIds) {
