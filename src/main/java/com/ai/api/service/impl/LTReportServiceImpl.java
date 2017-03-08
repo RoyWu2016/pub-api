324bb7565439b17fa794263958dc068b5d25203c
harveyteofilo <harveyteofilo@gmail.com>
2017-03-08 16:18:22 +0800

    AIMS-1089 | Modified to search orders for re-test and re-order
    # Conflicts:
    #	src/main/java/com/ai/api/controller/impl/LTOrderImpl.java
    #	src/main/java/com/ai/api/service/impl/LTOrderServiceImpl.java

--- src/main/java/com/ai/api/service/impl/LTReportServiceImpl.java
+++ src/main/java/com/ai/api/service/impl/LTReportServiceImpl.java
@@ -66,7 +66,10 @@ public class LTReportServiceImpl implements LTReportService {
 		    logger.info("use incorrect userId["+userId+"] to search LT orders");
 			throw new AIException("incorrect userId");
 		}
-		return ltOrderDao.searchLTOrders(companyId, OrderStatus.COMPLETED_CODE, pageNumber, pageSize, Sort.Direction.DESC.name().toLowerCase());
+		Map<String, Object> searchParams = new HashMap<String, Object>();
+		searchParams.put("clientId", companyId);
+		searchParams.put("orderStatus", OrderStatus.COMPLETED_CODE);
+		return ltOrderDao.searchLTOrders(searchParams, pageNumber, pageSize, Sort.Direction.DESC.name().toLowerCase());
 	}
 
 	@Override
