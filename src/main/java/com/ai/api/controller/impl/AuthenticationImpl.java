b086e605590b4485ad9fd54089f8021112525110
Roy Wu <Roy Wu@ldj-PC>
2017-03-09 11:08:07 +0800

    add swagger comment for checklist and authentication api

--- src/main/java/com/ai/api/controller/impl/AuthenticationImpl.java
+++ src/main/java/com/ai/api/controller/impl/AuthenticationImpl.java
@@ -18,9 +18,13 @@ import com.ai.commons.Consts;
 import com.ai.commons.HttpUtil;
 import com.ai.commons.StringUtils;
 import com.ai.commons.beans.ServiceCallResult;
+import com.ai.commons.beans.checklist.vo.CKLDefectVO;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,7 +35,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /***************************************************************************
- *<PRE>
+ * <PRE>
  *  Project Name    : api
  *
  *  Package Name    : com.ai.api.controller.impl
@@ -47,16 +51,17 @@ import org.springframework.web.bind.annotation.RestController;
  *
  *  History         : TODO
  *
- *</PRE>
+ * </PRE>
  ***************************************************************************/
 
 @RestController
-@Api(tags = {"Authentication"}, description = "Authentication tokens APIs")
+@Api(tags = { "Authentication" }, description = "Authentication tokens APIs")
 public class AuthenticationImpl implements Authentication {
 	private static final Logger logger = LoggerFactory.getLogger(AuthenticationImpl.class);
 
 	@Autowired
-	SSOUserServiceDao ssoUserServiceDao;  //Service which will do all data retrieval/manipulation work
+	SSOUserServiceDao ssoUserServiceDao; // Service which will do all data
+											// retrieval/manipulation work
 
 	@Autowired
 	UserService userService;
@@ -64,85 +69,89 @@ public class AuthenticationImpl implements Authentication {
 	AuthenticationService authenticationService;
 
 	@Override
-	@RequestMapping(method = RequestMethod.POST, value = "/auth/token")
 	@ResponseBody
-	public String userLogin( @RequestBody LoginBean loginBean,HttpServletRequest request, HttpServletResponse response)
-			throws JsonProcessingException {
+	@RequestMapping(method = RequestMethod.POST, value = "/auth/token")
+	@ApiOperation(value = "User Login Then Get Api Token", response = String.class)
+	public String userLogin(@ApiParam(required = true) @RequestBody LoginBean loginBean, HttpServletRequest request,
+			HttpServletResponse response) throws JsonProcessingException {
 		logger.info("user login ......");
-		//user can be client or employee
+		// user can be client or employee
 		String account = loginBean.getAccount();
-		//password should be in MD5 format
+		// password should be in MD5 format
 		String password = loginBean.getPassword();
 		String userType = loginBean.getUserType();
-        logger.info("account:"+account);
-        logger.info("userType:"+userType);
+		logger.info("account:" + account);
+		logger.info("userType:" + userType);
 		ServiceCallResult result = new ServiceCallResult();
 		ObjectMapper mapper = new ObjectMapper();
 
-		if ((StringUtils.isBlank(account))|| (StringUtils.isBlank(password))) {
+		if ((StringUtils.isBlank(account)) || (StringUtils.isBlank(password))) {
 			result.setStatusCode(HttpServletResponse.SC_FORBIDDEN);
-			result.setResponseString("");//username and password can not be null
+			result.setResponseString("");// username and password can not be
+											// null
 			result.setReasonPhase("username/password empty");
 			return mapper.writeValueAsString(result);
 		}
 
-		if (! Consts.Http.USER_TYPES.contains(userType)) {
+		if (!Consts.Http.USER_TYPES.contains(userType)) {
 			result.setStatusCode(HttpServletResponse.SC_FORBIDDEN);
 			result.setResponseString("");
 			result.setReasonPhase("wrong user type: " + userType);
 			return mapper.writeValueAsString(result);
 		}
 
-		//check api access token
+		// check api access token
 		boolean validateResult = HttpUtil.validatePublicAPICallToken(request);
-        logger.info("validate token result :"+validateResult);
+		logger.info("validate token result :" + validateResult);
 		if (!validateResult) {
 			result.setStatusCode(HttpServletResponse.SC_FORBIDDEN);
 			result.setResponseString("");
 			result.setReasonPhase("AI API call token not present or invalid for login.");
 			return mapper.writeValueAsString(result);
 		}
-        logger.info("start to get user from user-service --> verify pw --> generate tokenSession");
-        result = authenticationService.userLogin(account,password,userType,request);
-		logger.info("user login result: "+result.getResponseString());
+		logger.info("start to get user from user-service --> verify pw --> generate tokenSession");
+		result = authenticationService.userLogin(account, password, userType, request);
+		logger.info("user login result: " + result.getResponseString());
 		return mapper.writeValueAsString(result);
 	}
 
-
 	@Override
-	@RequestMapping(method = RequestMethod.PUT, value = "/auth/refresh-token")
 	@ResponseBody
+	@RequestMapping(method = RequestMethod.PUT, value = "/auth/refresh-token")
+	@ApiOperation(value = "Refresh Api Token", response = String.class)
 	public String refreshAPIToken(HttpServletRequest request, HttpServletResponse response)
 			throws JsonProcessingException {
 
 		logger.info("refresh token ...........");
 		ObjectMapper mapper = new ObjectMapper();
 		ServiceCallResult result = ssoUserServiceDao.refreshAPIToken(request, response);
-		logger.info("refresh token result: "+result.getResponseString());
+		logger.info("refresh token result: " + result.getResponseString());
 		return mapper.writeValueAsString(result);
 	}
 
 	@Override
-	@RequestMapping(method = RequestMethod.PUT, value = "/auth/remove-token")
 	@ResponseBody
+	@RequestMapping(method = RequestMethod.PUT, value = "/auth/remove-token")
+	@ApiOperation(value = "Remove Api Token", response = String.class)
 	public String removeAPIToken(HttpServletRequest request, HttpServletResponse response)
 			throws JsonProcessingException {
 		ObjectMapper mapper = new ObjectMapper();
-        logger.info("remove token ...........");
+		logger.info("remove token ...........");
 		ServiceCallResult result = authenticationService.removeAPIToken(request, response);
-		logger.info("remove token result: "+result.getResponseString());
+		logger.info("remove token result: " + result.getResponseString());
 		return mapper.writeValueAsString(result);
 	}
-	
+
 	@Override
-	@RequestMapping(method = RequestMethod.GET, value = "/auth/verify-token")
 	@ResponseBody
+	@RequestMapping(method = RequestMethod.GET, value = "/auth/verify-token")
+	@ApiOperation(value = "Verify Public Api Token", response = String.class)
 	public String verifyPublicAPIToken(HttpServletRequest request, HttpServletResponse response)
-		throws JsonProcessingException {
+			throws JsonProcessingException {
 		ObjectMapper mapper = new ObjectMapper();
 		logger.info("verify token ...........");
 		ServiceCallResult result = ssoUserServiceDao.verifyAPIToken(request, response);
-		logger.info("verify token result: "+result.getResponseString());
+		logger.info("verify token result: " + result.getResponseString());
 		return mapper.writeValueAsString(result);
 	}
 
