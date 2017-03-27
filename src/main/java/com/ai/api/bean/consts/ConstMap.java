ecb9054d8ea7a9316bc34560fb6daa8ee19d73be
Roy Wu <Roy Wu@ldj-PC>
2017-03-27 11:45:46 +0800

    COMMIT AI-3831 | optimized the return bean of get employee api

--- src/main/java/com/ai/api/bean/consts/ConstMap.java
+++ src/main/java/com/ai/api/bean/consts/ConstMap.java
@@ -2,8 +2,14 @@ package com.ai.api.bean.consts;
 
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
+
+import com.ai.api.bean.EmployeeBean;
+import com.ai.api.bean.EmployeeGroup;
+import com.ai.api.bean.EmployeeRole;
+import com.ai.commons.beans.audit.api.ApiEmployeeBean;
 
 /***************************************************************************
  * <PRE>
@@ -29,6 +35,26 @@ public class ConstMap {
 	public final static Map<String, String> serviceTypeMap = new HashMap();
 	public final static Set<String> DOC_TYPE = new HashSet<String>();
 	public final static Set<String> STATUS = new HashSet<String>();
+
+	static HashSet<String> AIMS = new HashSet<String>();
+	static HashSet<String> Audit = new HashSet<String>();
+	static HashSet<String> Checklist = new HashSet<String>();
+	static HashSet<String> CloudReport = new HashSet<String>();
+	static HashSet<String> Customer = new HashSet<String>();
+	static HashSet<String> Factory_Portal = new HashSet<String>();
+	static HashSet<String> GI = new HashSet<String>();
+	static HashSet<String> IP_Generation = new HashSet<String>();
+	static HashSet<String> IRP = new HashSet<String>();
+	static HashSet<String> Inspection = new HashSet<String>();
+	static HashSet<String> InspectorApp = new HashSet<String>();
+	static HashSet<String> Mail_Console = new HashSet<String>();
+	static HashSet<String> PROG = new HashSet<String>();
+	static HashSet<String> Program = new HashSet<String>();
+	static HashSet<String> Report_Portal_Inspector = new HashSet<String>();
+	static HashSet<String> Report_Portal_Supervisor = new HashSet<String>();
+	static HashSet<String> SSO_Management = new HashSet<String>();
+	static HashSet<String> Sales_Management = new HashSet<String>();
+	static HashSet<String> Sample = new HashSet<String>();
 
 	static {
 		bucketMap.put("ACCESS_MAP", "access-map");
@@ -60,7 +86,7 @@ public class ConstMap {
 		serviceTypeMap.put("ea", "9"); // Ethical Audit, formerly social audit
 		serviceTypeMap.put("ctpat", "5,51");
 		serviceTypeMap.put("stra", "9,91");
-		
+
 		DOC_TYPE.add("ACCESS_MAP");
 		DOC_TYPE.add("CHECKLIST_TEST");
 		DOC_TYPE.add("CHECKLIST_EXPECTED_DEFECT");
@@ -73,7 +99,7 @@ public class ConstMap {
 		DOC_TYPE.add("ORDER_ATT");
 		DOC_TYPE.add("AUDIT_PREVIEW_DOC");
 		DOC_TYPE.add("PROD_REPORT_PICTURE");
-		
+
 		STATUS.add("15");
 		STATUS.add("17");
 		STATUS.add("20");
@@ -84,5 +110,98 @@ public class ConstMap {
 		STATUS.add("40");
 		STATUS.add("50");
 		STATUS.add("60");
+	}
+
+	public static ApiEmployeeBean convert2ApiEmployeeBean(EmployeeBean source) {
+		ApiEmployeeBean apiEmployeeBean = new ApiEmployeeBean();
+		apiEmployeeBean.setDepartmentId(source.getDepartmentId());
+		apiEmployeeBean.setDepartmentName(source.getDepartmentName());
+		apiEmployeeBean.setEmail(source.getEmail());
+		apiEmployeeBean.setEmployeeType(source.getEmployeeType());
+		apiEmployeeBean.setFirstName(source.getFirstName());
+		apiEmployeeBean.setLastName(source.getLastName());
+		apiEmployeeBean.setUserId(source.getUserId());
+		apiEmployeeBean.setPhone(source.getPhone());
+		apiEmployeeBean.setPosition(source.getPosition());
+		apiEmployeeBean.setStatus(source.getStatus());
+
+		if (null != source.getRoles()) {
+			convert2Roles(apiEmployeeBean.getRoles(), source.getRoles());
+		}
+
+		if (null != source.getGroups()) {
+			for (EmployeeGroup each : source.getGroups()) {
+				if (null != each.getRoles()) {
+					convert2Roles(apiEmployeeBean.getRoles(), each.getRoles());
+				}
+			}
+		}
+
+		return apiEmployeeBean;
+
+	}
+
+	private static void convert2Roles(Map<String, HashSet<String>> result, List<EmployeeRole> sources) {
+		// TODO Auto-generated method stub
+		for (EmployeeRole role : sources) {
+			if ("AIMS".equalsIgnoreCase(role.getModuleName())) {
+				AIMS.add(role.getDisplayName());
+				result.put(role.getModuleName(), AIMS);
+			} else if ("Audit".equalsIgnoreCase(role.getModuleName())) {
+				Audit.add(role.getDisplayName());
+				result.put(role.getModuleName(), Audit);
+			} else if ("Checklist".equalsIgnoreCase(role.getModuleName())) {
+				Checklist.add(role.getDisplayName());
+				result.put(role.getModuleName(), Checklist);
+			} else if ("CloudReport".equalsIgnoreCase(role.getModuleName())) {
+				CloudReport.add(role.getDisplayName());
+				result.put(role.getModuleName(), CloudReport);
+			} else if ("Customer".equalsIgnoreCase(role.getModuleName())) {
+				Customer.add(role.getDisplayName());
+				result.put(role.getModuleName(), Customer);
+			} else if ("Factory-Portal".equalsIgnoreCase(role.getModuleName())) {
+				Factory_Portal.add(role.getDisplayName());
+				result.put(role.getModuleName(), Factory_Portal);
+			} else if ("GI".equalsIgnoreCase(role.getModuleName())) {
+				GI.add(role.getDisplayName());
+				result.put(role.getModuleName(), GI);
+			} else if ("IP Generation".equalsIgnoreCase(role.getModuleName())) {
+				IP_Generation.add(role.getDisplayName());
+				result.put(role.getModuleName(), IP_Generation);
+			} else if ("IRP".equalsIgnoreCase(role.getModuleName())) {
+				IRP.add(role.getDisplayName());
+				result.put(role.getModuleName(), IRP);
+			} else if ("Inspection".equalsIgnoreCase(role.getModuleName())) {
+				Inspection.add(role.getDisplayName());
+				result.put(role.getModuleName(), Inspection);
+			} else if ("InspectorApp".equalsIgnoreCase(role.getModuleName())) {
+				InspectorApp.add(role.getDisplayName());
+				result.put(role.getModuleName(), InspectorApp);
+			} else if ("Mail Console".equalsIgnoreCase(role.getModuleName())) {
+				Mail_Console.add(role.getDisplayName());
+				result.put(role.getModuleName(), Mail_Console);
+			} else if ("PROG".equalsIgnoreCase(role.getModuleName())) {
+				PROG.add(role.getDisplayName());
+				result.put(role.getModuleName(), PROG);
+			} else if ("Program".equalsIgnoreCase(role.getModuleName())) {
+				Program.add(role.getDisplayName());
+				result.put(role.getModuleName(), Program);
+			} else if ("Report Portal-Inspector".equalsIgnoreCase(role.getModuleName())) {
+				Report_Portal_Inspector.add(role.getDisplayName());
+				result.put(role.getModuleName(), Report_Portal_Inspector);
+			} else if ("Report Portal-Supervisor".equalsIgnoreCase(role.getModuleName())) {
+				Report_Portal_Supervisor.add(role.getDisplayName());
+				result.put(role.getModuleName(), Report_Portal_Supervisor);
+			} else if ("SSO Management".equalsIgnoreCase(role.getModuleName())) {
+				SSO_Management.add(role.getDisplayName());
+				result.put(role.getModuleName(), SSO_Management);
+			} else if ("Sales Management".equalsIgnoreCase(role.getModuleName())) {
+				Sales_Management.add(role.getDisplayName());
+				result.put(role.getModuleName(), Sales_Management);
+			} else if ("Sample".equalsIgnoreCase(role.getModuleName())) {
+				Sample.add(role.getDisplayName());
+				result.put(role.getModuleName(), Sample);
+			}
+		}
 	}
 }
