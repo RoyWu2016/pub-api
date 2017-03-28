a7bac89c5e0c74c11df1f62ad475f9eb5feedd31
Roy Wu <Roy Wu@ldj-PC>
2017-03-28 10:44:13 +0800

    compelted the swagger comments

--- src/main/java/com/ai/api/controller/impl/DraftImpl.java
+++ src/main/java/com/ai/api/controller/impl/DraftImpl.java
@@ -28,6 +28,7 @@ import com.ai.commons.beans.psi.InspectionProductBookingBean;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 
 /***************************************************************************
  * <PRE>
@@ -48,7 +49,7 @@ import io.swagger.annotations.ApiOperation;
  ***************************************************************************/
 
 @RestController
-@Api(tags = {"Draft"}, description = "Draft booking APIs")
+@Api(tags = { "Draft" }, description = "Draft booking APIs")
 public class DraftImpl implements Draft {
 
 	protected Logger logger = LoggerFactory.getLogger(DraftImpl.class);
@@ -60,7 +61,8 @@ public class DraftImpl implements Draft {
 	@TokenSecured
 	@RequestMapping(value = "/user/{userId}/drafts/{draftIds}", method = RequestMethod.DELETE)
 	@ApiOperation(value = "Delete Draft API", response = Boolean.class)
-	public ResponseEntity<Boolean> deleteDrafts(@PathVariable("userId") String userId, @PathVariable("draftIds") String draftIds) {
+	public ResponseEntity<Boolean> deleteDrafts(@ApiParam(required = true) @PathVariable("userId") String userId,
+			@ApiParam(required = true) @PathVariable("draftIds") String draftIds) {
 		try {
 			logger.info("deleteDrafts. . .");
 			boolean result = draftService.deleteDraftFromPsi(userId, draftIds);
@@ -76,8 +78,10 @@ public class DraftImpl implements Draft {
 	@Override
 	@TokenSecured
 	@RequestMapping(value = "/user/{userId}/draft", method = RequestMethod.POST)
-	public ResponseEntity<InspectionBookingBean> createDraft(@PathVariable("userId") String userId,
-	                                                         @RequestParam(value = "serviceType", required = true) String serviceType) {
+	@ApiOperation(value = "Create Draft API", response = InspectionBookingBean.class)
+	public ResponseEntity<InspectionBookingBean> createDraft(
+			@ApiParam(required = true) @PathVariable("userId") String userId,
+			@ApiParam(required = true) @RequestParam(value = "serviceType", required = true) String serviceType) {
 		if (userId == null || userId.isEmpty()) {
 			logger.error("userId:" + userId);
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
@@ -96,12 +100,14 @@ public class DraftImpl implements Draft {
 	@Override
 	@TokenSecured
 	@RequestMapping(value = "/user/{userId}/draft/previous-psi-order/{orderId}", method = RequestMethod.POST)
+	@ApiOperation(value = "Create Draft From Previous Order API", response = InspectionBookingBean.class)
 	public ResponseEntity<InspectionBookingBean> createDraftFromPreviousOrder(
-														@PathVariable("userId") String userId,
-													    @PathVariable("orderId") String orderId,
-													    @RequestParam(value = "service-type", required = false) String serviceType ) {
+			@ApiParam(required = true) @PathVariable("userId") String userId,
+			@ApiParam(required = true) @PathVariable("orderId") String orderId,
+			@ApiParam(required = false) @RequestParam(value = "service-type", required = false) String serviceType) {
 		try {
-			InspectionBookingBean newDraft = draftService.createDraftFromPreviousOrder(userId, orderId, serviceType.toLowerCase());
+			InspectionBookingBean newDraft = draftService.createDraftFromPreviousOrder(userId, orderId,
+					serviceType.toLowerCase());
 			if (null != newDraft) {
 				return new ResponseEntity<>(newDraft, HttpStatus.OK);
 			}
@@ -114,8 +120,9 @@ public class DraftImpl implements Draft {
 	@Override
 	@TokenSecured
 	@RequestMapping(value = "/user/{userId}/draft/{draftId}", method = RequestMethod.GET)
-	public ResponseEntity<InspectionBookingBean> getDraft(@PathVariable final String userId,
-	                                                      @PathVariable final String draftId) {
+	@ApiOperation(value = "Get User Draft API", response = InspectionBookingBean.class)
+	public ResponseEntity<InspectionBookingBean> getDraft(@ApiParam(required = true) @PathVariable final String userId,
+			@ApiParam(required = true) @PathVariable final String draftId) {
 
 		if (userId == null || userId.isEmpty() || draftId == null || draftId.isEmpty()) {
 			logger.error("userId:" + userId + ", draftId:" + draftId + " can't be null!");
@@ -138,12 +145,12 @@ public class DraftImpl implements Draft {
 	@Override
 	@TokenSecured
 	@RequestMapping(value = "/user/{userId}/draft/{draftId}", method = RequestMethod.PUT)
-	public ResponseEntity<Boolean> saveDraft(@PathVariable("userId") String userId,
-	                                         @PathVariable("draftId") String draftId,
-	                                         @RequestBody InspectionBookingBean draft) {
+	@ApiOperation(value = "Save User Draft API", response = Boolean.class)
+	public ResponseEntity<Boolean> saveDraft(@ApiParam(required = true) @PathVariable("userId") String userId,
+			@ApiParam(required = true) @PathVariable("draftId") String draftId,
+			@ApiParam(required = true) @RequestBody InspectionBookingBean draft) {
 		if (userId == null || userId.isEmpty() || draftId == null || draftId.isEmpty() || draft == null) {
-			logger.error("userId:" + userId + ", draftId:" + draftId + ", draft:" + draft
-					+ " can't be null!");
+			logger.error("userId:" + userId + ", draftId:" + draftId + ", draft:" + draft + " can't be null!");
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 		}
 		try {
@@ -161,8 +168,10 @@ public class DraftImpl implements Draft {
 	@Override
 	@TokenSecured
 	@RequestMapping(value = "/user/{userId}/draft/{draftId}/product", method = RequestMethod.POST)
-	public ResponseEntity<InspectionProductBookingBean> addProduct(@PathVariable("userId") String userId,
-	                                                               @PathVariable("draftId") String draftId) {
+	@ApiOperation(value = "Add Product API", response = InspectionProductBookingBean.class)
+	public ResponseEntity<InspectionProductBookingBean> addProduct(
+			@ApiParam(required = true) @PathVariable("userId") String userId,
+			@ApiParam(required = true) @PathVariable("draftId") String draftId) {
 		try {
 			InspectionProductBookingBean product = draftService.addProduct(userId, draftId);
 			if (null != product) {
@@ -174,13 +183,13 @@ public class DraftImpl implements Draft {
 		return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 
-
 	@Override
 	@TokenSecured
 	@RequestMapping(value = "/user/{userId}/draft/{draftId}/product/{productId}", method = RequestMethod.DELETE)
-	public ResponseEntity<Boolean> deleteProduct(@PathVariable("userId") String userId,
-	                                             @PathVariable("draftId") String draftId,
-	                                             @PathVariable("productId") String productId) {
+	@ApiOperation(value = "Delete Product API", response = Boolean.class)
+	public ResponseEntity<Boolean> deleteProduct(@ApiParam(required = true) @PathVariable("userId") String userId,
+			@ApiParam(required = true) @PathVariable("draftId") String draftId,
+			@ApiParam(required = true) @PathVariable("productId") String productId) {
 		try {
 			boolean result = draftService.deleteProduct(userId, productId);
 			if (result) {
@@ -194,20 +203,21 @@ public class DraftImpl implements Draft {
 
 	@Override
 	@TokenSecured
-	@RequestMapping(value = "/user/{userId}/draft/{draftId}/sampling-level/{samplingLevel}/price",
-			method = RequestMethod.GET)
+	@RequestMapping(value = "/user/{userId}/draft/{draftId}/sampling-level/{samplingLevel}/price", method = RequestMethod.GET)
+	@ApiOperation(value = "Calculate Pricing API", response = OrderPriceMandayViewBean.class)
 	public ResponseEntity<OrderPriceMandayViewBean> calculatePricing(
-					@PathVariable("userId") String userId,
-					@PathVariable("draftId") String draftId,
-					@PathVariable("samplingLevel") String samplingLevel,
-					@RequestParam(value = "measurementSamplingSize", required = false, defaultValue = "") String measurementSamplingSize) {
+			@ApiParam(required = true) @PathVariable("userId") String userId,
+			@ApiParam(required = true) @PathVariable("draftId") String draftId,
+			@ApiParam(required = true) @PathVariable("samplingLevel") String samplingLevel,
+			@ApiParam(required = false) @RequestParam(value = "measurementSamplingSize", required = false, defaultValue = "") String measurementSamplingSize) {
 		if (userId == null || draftId == null || samplingLevel == null) {
 			logger.error("userId:" + userId + ", draftId:" + draftId + ", sample level:" + samplingLevel
 					+ " can't be null!");
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 		}
 		try {
-			OrderPriceMandayViewBean newDraft = draftService.calculatePricing(userId, draftId, samplingLevel, measurementSamplingSize);
+			OrderPriceMandayViewBean newDraft = draftService.calculatePricing(userId, draftId, samplingLevel,
+					measurementSamplingSize);
 			if (null != newDraft) {
 				return new ResponseEntity<>(newDraft, HttpStatus.OK);
 			} else {
@@ -220,20 +230,22 @@ public class DraftImpl implements Draft {
 		}
 	}
 
-
 	@Override
 	@TokenSecured
 	@RequestMapping(value = "/user/{userId}/psi-drafts", method = RequestMethod.GET)
-	public ResponseEntity<List<DraftOrder>> searchDraft(@PathVariable("userId") String userId,
-	                                                    @RequestParam(value = "service-type", defaultValue = "1,2,3,4,6") String serviceType,
-	                                                    @RequestParam(value = "start", required = false, defaultValue = "") String startDate,
-	                                                    @RequestParam(value = "end", required = false, defaultValue = "") String endDate,
-	                                                    @RequestParam(value = "keyword", required = false, defaultValue = "") String keyword,
-	                                                    @RequestParam(value = "page", required = false, defaultValue = "1") String pageNumber,
-	                                                    @RequestParam(value = "page-size", required = false, defaultValue = "20") String pageSize) {
+	@ApiOperation(value = "Search User Drafts API", response = DraftOrder.class)
+	public ResponseEntity<List<DraftOrder>> searchDraft(
+			@ApiParam(required = true) @PathVariable("userId") String userId,
+			@ApiParam(required = true) @RequestParam(value = "service-type", defaultValue = "1,2,3,4,6") String serviceType,
+			@ApiParam(required = false) @RequestParam(value = "start", required = false, defaultValue = "") String startDate,
+			@ApiParam(required = false) @RequestParam(value = "end", required = false, defaultValue = "") String endDate,
+			@ApiParam(required = false) @RequestParam(value = "keyword", required = false, defaultValue = "") String keyword,
+			@ApiParam(required = false) @RequestParam(value = "page", required = false, defaultValue = "1") String pageNumber,
+			@ApiParam(required = false) @RequestParam(value = "page-size", required = false, defaultValue = "20") String pageSize) {
 		try {
-			List<DraftOrder> draftList = draftService.searchDraft(userId, serviceType, startDate, endDate, keyword, pageNumber, pageSize);
-			//if not data found, just return 200 with empty list
+			List<DraftOrder> draftList = draftService.searchDraft(userId, serviceType, startDate, endDate, keyword,
+					pageNumber, pageSize);
+			// if not data found, just return 200 with empty list
 			return new ResponseEntity<>(draftList, HttpStatus.OK);
 		} catch (Exception e) {
 			logger.error("get draft search error: " + ExceptionUtils.getFullStackTrace(e));
@@ -243,105 +255,90 @@ public class DraftImpl implements Draft {
 	}
 
 	/*
-	@Override
-	@TokenSecured
-	@RequestMapping(value = "/user/{userId}/draft/{draftId}/products", method = RequestMethod.PUT)
-	public ResponseEntity<Boolean> saveProducts(
-			@PathVariable("userId") String userId,
-			@PathVariable("draftId") String draftId,
-			@RequestBody InspectionDraftProductsAndStepBean inspectionDraftProductsAndStepBean) {
-		try {
-			logger.info("Checking what non-duplicate product id should be delete before update multiple products: ");
-			InspectionBookingBean draft = draftService.getDraft(userId, draftId);
-			if (null == draft) {
-				return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-			}
-			List<InspectionProductBookingBean> list = draft.getProducts();
-			if (list.size() > inspectionDraftProductsAndStepBean.getDraftProductsList().size()) {
-
-				List<String> needTodeleteProductIdList = new ArrayList<String>();
-				List<String> inputProductIdList = new ArrayList<String>();
-				for (InspectionProductBookingBean each : list) {
-					needTodeleteProductIdList.add(each.getDraftProductId());
-				}
-				for (InspectionProductBookingBean each : inspectionDraftProductsAndStepBean.getDraftProductsList()) {
-					inputProductIdList.add(each.getDraftProductId());
-				}
-				needTodeleteProductIdList.removeAll(inputProductIdList);
-
-				for (String id : needTodeleteProductIdList) {
-					logger.debug("Deleting the product id: " + id);
-					if (!draftService.deleteProduct(userId, id)) {
-						return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-					}
-				}
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		for (InspectionProductBookingBean each : inspectionDraftProductsAndStepBean.getDraftProductsList()) {
-			try {
-				boolean result = draftService.saveProduct(userId, each);
-				if (!result) {
-					return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-		try {
-			boolean result = draftService.saveDraftStep(userId, draftId, inspectionDraftProductsAndStepBean.getDraftSteps());
-			if (!result) {
-				return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return new ResponseEntity<>(HttpStatus.OK);
-	}
-
-	@Override
-	@TokenSecured
-	@RequestMapping(value = "/user/{userId}/draft/{draftId}/step", method = RequestMethod.PUT)
-	public ResponseEntity<Boolean> saveDraftStep(
-			@PathVariable("userId") String userId,
-			@PathVariable("draftId") String draftId,
-			@RequestBody List<DraftStepBean> draftSteps) {
-		try {
-			boolean result = draftService.saveDraftStep(userId, draftId, draftSteps);
-			if (result) {
-				return new ResponseEntity<>(HttpStatus.OK);
-			} else {
-				return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
-
-	@Override
-	@TokenSecured
-	@RequestMapping(value = "/user/{userId}/draft/{draftId}/product/{productId}", method = RequestMethod.PUT)
-	public ResponseEntity<Boolean> saveProduct(@PathVariable("userId") String userId,
-	                                           @PathVariable("draftId") String draftId,
-	                                           @PathVariable("productId") String productId,
-	                                           @RequestBody InspectionProductBookingBean draftProduct) {
-		try {
-			draftProduct.setDraftProductId(productId);
-			draftProduct.setDraftId(draftId);
-			boolean result = draftService.saveProduct(userId, draftProduct);
-			if (result) {
-				return new ResponseEntity<>(HttpStatus.OK);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-	}
-	*/
+	 * @Override
+	 * 
+	 * @TokenSecured
+	 * 
+	 * @RequestMapping(value = "/user/{userId}/draft/{draftId}/products", method
+	 * = RequestMethod.PUT) public ResponseEntity<Boolean> saveProducts(
+	 * 
+	 * @PathVariable("userId") String userId,
+	 * 
+	 * @PathVariable("draftId") String draftId,
+	 * 
+	 * @RequestBody InspectionDraftProductsAndStepBean
+	 * inspectionDraftProductsAndStepBean) { try { logger.
+	 * info("Checking what non-duplicate product id should be delete before update multiple products: "
+	 * ); InspectionBookingBean draft = draftService.getDraft(userId, draftId);
+	 * if (null == draft) { return new
+	 * ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR); }
+	 * List<InspectionProductBookingBean> list = draft.getProducts(); if
+	 * (list.size() >
+	 * inspectionDraftProductsAndStepBean.getDraftProductsList().size()) {
+	 * 
+	 * List<String> needTodeleteProductIdList = new ArrayList<String>();
+	 * List<String> inputProductIdList = new ArrayList<String>(); for
+	 * (InspectionProductBookingBean each : list) {
+	 * needTodeleteProductIdList.add(each.getDraftProductId()); } for
+	 * (InspectionProductBookingBean each :
+	 * inspectionDraftProductsAndStepBean.getDraftProductsList()) {
+	 * inputProductIdList.add(each.getDraftProductId()); }
+	 * needTodeleteProductIdList.removeAll(inputProductIdList);
+	 * 
+	 * for (String id : needTodeleteProductIdList) {
+	 * logger.debug("Deleting the product id: " + id); if
+	 * (!draftService.deleteProduct(userId, id)) { return new
+	 * ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR); } } } } catch
+	 * (Exception e) { e.printStackTrace(); }
+	 * 
+	 * for (InspectionProductBookingBean each :
+	 * inspectionDraftProductsAndStepBean.getDraftProductsList()) { try {
+	 * boolean result = draftService.saveProduct(userId, each); if (!result) {
+	 * return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR); } } catch
+	 * (Exception e) { e.printStackTrace(); } } try { boolean result =
+	 * draftService.saveDraftStep(userId, draftId,
+	 * inspectionDraftProductsAndStepBean.getDraftSteps()); if (!result) {
+	 * return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR); } } catch
+	 * (Exception e) { e.printStackTrace(); } return new
+	 * ResponseEntity<>(HttpStatus.OK); }
+	 * 
+	 * @Override
+	 * 
+	 * @TokenSecured
+	 * 
+	 * @RequestMapping(value = "/user/{userId}/draft/{draftId}/step", method =
+	 * RequestMethod.PUT) public ResponseEntity<Boolean> saveDraftStep(
+	 * 
+	 * @PathVariable("userId") String userId,
+	 * 
+	 * @PathVariable("draftId") String draftId,
+	 * 
+	 * @RequestBody List<DraftStepBean> draftSteps) { try { boolean result =
+	 * draftService.saveDraftStep(userId, draftId, draftSteps); if (result) {
+	 * return new ResponseEntity<>(HttpStatus.OK); } else { return new
+	 * ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR); } } catch (Exception
+	 * e) { e.printStackTrace(); } return null; }
+	 * 
+	 * @Override
+	 * 
+	 * @TokenSecured
+	 * 
+	 * @RequestMapping(value =
+	 * "/user/{userId}/draft/{draftId}/product/{productId}", method =
+	 * RequestMethod.PUT) public ResponseEntity<Boolean>
+	 * saveProduct(@PathVariable("userId") String userId,
+	 * 
+	 * @PathVariable("draftId") String draftId,
+	 * 
+	 * @PathVariable("productId") String productId,
+	 * 
+	 * @RequestBody InspectionProductBookingBean draftProduct) { try {
+	 * draftProduct.setDraftProductId(productId);
+	 * draftProduct.setDraftId(draftId); boolean result =
+	 * draftService.saveProduct(userId, draftProduct); if (result) { return new
+	 * ResponseEntity<>(HttpStatus.OK); } } catch (Exception e) {
+	 * e.printStackTrace(); } return new
+	 * ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR); }
+	 */
 
 }
-
- 
