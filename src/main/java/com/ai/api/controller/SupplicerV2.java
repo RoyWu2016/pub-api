ecf47976a28a05fb43e7616c7a4c87374d1ad32c
Roy Wu <Roy Wu@ldj-PC>
2017-04-18 11:11:10 +0800

    add all apiCallResult to ai-3901

--- src/main/java/com/ai/api/controller/SupplicerV2.java
+++ src/main/java/com/ai/api/controller/SupplicerV2.java
@@ -4,11 +4,38 @@ import java.io.IOException;
 
 import org.springframework.http.ResponseEntity;
 
+import com.ai.api.bean.SupplierDetailBean;
 import com.ai.api.exception.AIException;
 import com.ai.commons.beans.ApiCallResult;
+import com.ai.commons.beans.psi.OrderFactoryBean;
+import com.ai.commons.beans.psi.api.ApiOrderFactoryBean;
 
 public interface SupplicerV2 {
 
 	ResponseEntity<ApiCallResult> getSuppliersByUserId(String userId) throws IOException, AIException;
+
+	ResponseEntity<ApiCallResult> getSupplierDetailInfoById(String userId, String supplierId)
+			throws IOException, AIException;
+
+	ResponseEntity<ApiCallResult> updateUserSupplierDetailInfo(String userId, String supplierId,
+			SupplierDetailBean supplierDetailBean) throws IOException, AIException;
+
+	ResponseEntity<ApiCallResult> deleteSuppliers(String userId, String supplierIds) throws IOException, AIException;
+
+	ResponseEntity<ApiCallResult> createSupplier(String userId, SupplierDetailBean supplierDetailBean)
+			throws IOException, AIException;
+
+	ResponseEntity<ApiCallResult> updateFactoryConfirm(String orderId, String password, String inspectionDateString,
+			String containReadyTime, OrderFactoryBean orderFactoryBean);
+
+	ResponseEntity<ApiCallResult> updateAuditFactoryConfirm(String orderId, String password, String auditDate,
+			String containReadyTime, ApiOrderFactoryBean orderFactoryBean);
+
+	ResponseEntity<ApiCallResult> updateSupplierDetailInfo(String orderId, String password,
+			SupplierDetailBean supplierDetailBean) throws IOException, AIException;
+
+	ResponseEntity<ApiCallResult> getFactoryConfirm(String orderId, String password);
+
+	ResponseEntity<ApiCallResult> getAuditFactoryConfirm(String orderId, String password);
 
 }
